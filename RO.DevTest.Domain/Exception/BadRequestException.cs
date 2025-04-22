770565237179a0605529a4893539cdc466f7dbf3
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-04-22 14:02:54 -0300

    feat(core): implement BadRequestException to standardize client error responses

--- RO.DevTest.Domain/Exception/BadRequestException.cs
+++ RO.DevTest.Domain/Exception/BadRequestException.cs
@@ -1,18 +1,38 @@
 ﻿using System.Net;
+using System.Linq;
+using System.Collections.Generic;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Identity;
 
 namespace RO.DevTest.Domain.Exception;
 
-// [TODO] Standardize requests
-// [TODO] Display a semantic error message to the client
 /// <summary>
-/// Returns a <see cref="HttpStatusCode.BadRequest"/> to
-/// the request
+/// Returns a <see cref="HttpStatusCode.BadRequest"/> to the request.
+/// Used to standardize error responses for bad requests.
 /// </summary>
-public class BadRequestException : ApiException {
+public class BadRequestException : ApiException
+{
     public override HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
-    public BadRequestException(IdentityResult result) : base(result) { }
-    public BadRequestException(string error) : base(error) { }
-    public BadRequestException(ValidationResult validationResult) : base(validationResult) { }
+
+    public IEnumerable<string> Errors { get; }
+
+    public BadRequestException(string error) : base(error)
+    {
+        Errors = new List<string> { error };
+    }
+
+    public BadRequestException(IEnumerable<string> errors) : base("One or more validation errors occurred.")
+    {
+        Errors = errors;
+    }
+
+    public BadRequestException(ValidationResult validationResult) 
+        : this(validationResult.Errors.Select(e => e.ErrorMessage))
+    {
+    }
+
+    public BadRequestException(IdentityResult result)
+        : this(result.Errors.Select(e => e.Description))
+    {
+    }
 }
