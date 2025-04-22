f57d8be2241c231e8cd64e7050fb9631078bc9a6
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-04-22 14:04:52 -0300

    feat(auth): implement login endpoint in AuthController using MediatR

--- RO.DevTest.WebApi/Controllers/AuthController.cs
+++ RO.DevTest.WebApi/Controllers/AuthController.cs
@@ -1,13 +1,28 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
+using RO.DevTest.Application.Features.Auth.Commands.LoginCommand;
 
 namespace RO.DevTest.WebApi.Controllers;
 
 [Route("api/auth")]
+[ApiController]
 [OpenApiTags("Auth")]
-public class AuthController(IMediator mediator) : Controller {
+public class AuthController(IMediator mediator) : ControllerBase
+{
     private readonly IMediator _mediator = mediator;
 
-    ///[TODO] - CREATE LOGIN HANDLER HERE 
+    /// <summary>
+    /// Realiza login de um usuário e retorna o token JWT.
+    /// </summary>
+    /// <param name="command">Credenciais de login.</param>
+    /// <returns>Token JWT e informações do usuário.</returns>
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Login([FromBody] LoginCommand command)
+    {
+        var response = await _mediator.Send(command);
+        return Ok(response);
+    }
 }
